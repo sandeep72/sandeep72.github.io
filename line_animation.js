bbb4bb0ca63f2ca0d11468fd1d0054ea48dec84f
sandeep72 <19sandeep.yadav@gmail.com>
2023-03-16 16:50:30 -0500

    push portfolio to git

---
+++ line_animation.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    particleground(document.getElementById('particles'), {
+        dotColor: '#EFF659',
+        lineColor: '#EFF659'
+    });
+
+}, false);
